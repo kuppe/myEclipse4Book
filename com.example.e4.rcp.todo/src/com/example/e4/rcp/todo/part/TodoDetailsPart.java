268a50629b65e04f5452188c24d70925740cd76d
Markus Alexander Kuppe <kuppe-github.com@lemmster.de>
2013-04-16 13:59:55 +0200

    Exercise 39 Build a SWT user interface (TodoDetailsPart)

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/part/TodoDetailsPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/part/TodoDetailsPart.java
@@ -1,4 +1,58 @@
 package com.example.e4.rcp.todo.part;
 
+import javax.annotation.PostConstruct;
+
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DateTime;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
 public class TodoDetailsPart {
+	private Text summary;
+	private Text description;
+	private Button isDone;
+	private DateTime dateTime;
+
+	@PostConstruct
+	public void createPartControl(Composite parent) {
+		GridLayout gl_parent = new GridLayout(2, false);
+		gl_parent.horizontalSpacing = 10;
+		parent.setLayout(gl_parent);
+
+		Label lblSummary = new Label(parent, SWT.NONE);
+		lblSummary.setText("Summary");
+
+		summary = new Text(parent, SWT.BORDER);
+		summary.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false,
+				1, 1));
+
+		Label lblDescription = new Label(parent, SWT.NONE);
+		lblDescription.setText("Description");
+
+		description = new Text(parent, SWT.BORDER | SWT.MULTI);
+		description.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true,
+				1, 1));
+
+		Label lblDueDate = new Label(parent, SWT.NONE);
+		lblDueDate.setText("Due Date");
+
+		dateTime = new DateTime(parent, SWT.BORDER);
+		dateTime.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false,
+				1, 1));
+		new Label(parent, SWT.NONE);
+
+		isDone = new Button(parent, SWT.CHECK);
+		isDone.setBounds(0, 0, 115, 24);
+		isDone.setText("Done");
+	}
+
+	@Focus
+	public void focus() {
+		this.summary.setFocus();
+	}
 }
--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/part/TodoOverview.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/part/TodoOverview.java
@@ -1,8 +1,14 @@
 package com.example.e4.rcp.todo.part;
 
-import javax.inject.Inject;
+import javax.annotation.PostConstruct;
 
+import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 
@@ -10,10 +16,31 @@ import com.example.e4.rcp.todo.model.ITodoService;
 
 public class TodoOverview {
 
-	@Inject
-	public TodoOverview(Composite parent, ITodoService model) {
-		int size = model.getTodos().size();
-		Label label = new Label(parent, SWT.NONE);
-		label.setText("Found todos #" + size);
+	private Label label;
+
+	@PostConstruct
+	public void createPart(Composite parent, final ITodoService model) {
+		GridLayout gl_parent = new GridLayout(3, false);
+		gl_parent.horizontalSpacing = 10;
+		parent.setLayout(gl_parent);
+
+		Button btnLoadData = new Button(parent, SWT.NONE);
+		btnLoadData.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				label.setText("Todos #" + model.getTodos().size());
+			}
+		});
+		btnLoadData.setText("Load Data");
+		label = new Label(parent, SWT.NONE);
+		label.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1,
+				1));
+		label.setText("Todos not yet loaded");
+		new Label(parent, SWT.NONE);
+	}
+
+	@Focus
+	public void focus() {
+		this.label.setFocus();
 	}
 }
