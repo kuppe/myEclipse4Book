f9cbb0f82e5396c4dd999fa98462fdeec947bf64
Markus Alexander Kuppe <kuppe-github.com@lemmster.de>
2013-04-16 17:02:45 +0200

    Exercise 45.2 Add a filter to the table

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/part/TodoOverview.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/part/TodoOverview.java
@@ -7,7 +7,11 @@ import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
@@ -16,6 +20,7 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.Text;
 
 import com.example.e4.rcp.todo.model.ITodoService;
 import com.example.e4.rcp.todo.model.Todo;
@@ -24,6 +29,25 @@ public class TodoOverview {
 
 	private Label label;
 	private TableViewer tv;
+	private Text text;
+
+	private static class MyViewerFilter extends ViewerFilter {
+		private String filter;
+
+		@Override
+		public boolean select(Viewer viewer, Object parentElement,
+				Object element) {
+			Todo t = (Todo) element;
+			if (t.getSummary().startsWith(filter)) {
+				return true;
+			}
+			return false;
+		}
+
+		public void setFilter(String filter) {
+			this.filter = filter;
+		}
+	}
 
 	@PostConstruct
 	public void createPart(Composite parent, final ITodoService model) {
@@ -44,6 +68,27 @@ public class TodoOverview {
 		label.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1,
 				1));
 		label.setText("Todos not yet loaded");
+
+		text = new Text(parent, SWT.BORDER | SWT.H_SCROLL | SWT.SEARCH
+				| SWT.CANCEL);
+		text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+		text.setMessage("Filter");
+		text.addModifyListener(new ModifyListener() {
+			private final MyViewerFilter viewerFilter = new MyViewerFilter();
+
+			@Override
+			public void modifyText(ModifyEvent e) {
+				Text text = (Text) e.getSource();
+				final String filter = text.getText();
+				if (filter != null && !filter.equals("")) {
+					tv.removeFilter(viewerFilter);
+					viewerFilter.setFilter(filter);
+					tv.addFilter(viewerFilter);
+				} else if (filter != null && filter.equals("")) {
+					tv.removeFilter(viewerFilter);
+				}
+			}
+		});
 
 		tv = new TableViewer(parent);
 		Table table = tv.getTable();
