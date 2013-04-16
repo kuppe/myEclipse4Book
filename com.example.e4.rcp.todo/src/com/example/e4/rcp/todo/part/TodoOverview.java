254675f36e9382bd62bbc6ebad483ce8704c2332
Markus Alexander Kuppe <kuppe-github.com@lemmster.de>
2013-04-16 10:48:12 +0200

    Exercise 23 Using dependency injection (to inject a composite into TodoOverview)

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/part/TodoOverview.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/part/TodoOverview.java
@@ -1,4 +1,13 @@
 package com.example.e4.rcp.todo.part;
 
+import javax.inject.Inject;
+
+import org.eclipse.swt.widgets.Composite;
+
 public class TodoOverview {
+
+	@Inject
+	public TodoOverview(Composite parent) {
+		System.out.println("Wohoo, got composite via DI");
+	}
 }
