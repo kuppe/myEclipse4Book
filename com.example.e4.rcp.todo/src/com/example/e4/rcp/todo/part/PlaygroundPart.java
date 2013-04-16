35ac494071470baa6ebb3d24fb77a4be4eb73be1
Markus Alexander Kuppe <kuppe-github.com@lemmster.de>
2013-04-16 10:51:41 +0200

    Exercise 24 Define part lifecycle with @Inject and @PostConstruct

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/part/PlaygroundPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/part/PlaygroundPart.java
@@ -1,4 +1,25 @@
 package com.example.e4.rcp.todo.part;
 
+import javax.annotation.PostConstruct;
+
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
 public class PlaygroundPart {
+
+	private Label label;
+
+	@PostConstruct
+	public void createUI(Composite parent) {
+		label = new Label(parent, SWT.NONE);
+		label.setText("A Label");
+	}
+
+	@Focus
+	public void setFocus() {
+		// Trigger SWT.Activate
+		label.setFocus();
+	}
 }
