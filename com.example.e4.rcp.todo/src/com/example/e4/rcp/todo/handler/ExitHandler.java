f733f18dd2fd6bc04253b1968168d45262f6ed82
Markus Alexander Kuppe <kuppe-github.com@lemmster.de>
2013-04-17 16:44:33 +0200

    Exercise 56.1 Confirmation dialog at exit

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handler/ExitHandler.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handler/ExitHandler.java
@@ -2,10 +2,16 @@ package com.example.e4.rcp.todo.handler;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.workbench.IWorkbench;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
 
 public class ExitHandler {
 	@Execute
-	public void execute(IWorkbench workbench) {
-		workbench.close();
+	public void execute(IWorkbench workbench, Shell shell) {
+		boolean dialog = MessageDialog.openConfirm(shell, "Close Application",
+				"Click ok to close the application");
+		if (dialog) {
+			workbench.close();
+		}
 	}
 }
