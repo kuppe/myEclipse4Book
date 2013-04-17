9b3aa29366e0c2de5b1a7b7ca626fa31c80c5c9b
Markus Alexander Kuppe <kuppe-github.com@lemmster.de>
2013-04-17 16:20:14 +0200

    Demo: Toyed with parameters (Have test handler print the parameter to console that is inject via @Named)

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handler/TestHandler.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handler/TestHandler.java
@@ -1,9 +1,16 @@
 package com.example.e4.rcp.todo.handler;
 
+import javax.inject.Named;
+
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.di.annotations.Optional;
 
 public class TestHandler {
+	// The Id is set in the application model for the corresponding command
+	private static final String COMMAND_PARAMETER_ID = "com.example.e4.rcp.todo.commandparameter.test";
+
 	@Execute
-	public void execute() {
+	public void execute(@Optional @Named(COMMAND_PARAMETER_ID) String str) {
+		System.out.println(str);
 	}
 }
