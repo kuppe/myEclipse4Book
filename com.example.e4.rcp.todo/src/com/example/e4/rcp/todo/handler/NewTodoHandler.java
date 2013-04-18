ccd0c676dd57ac4d0ebdf6e34d3af60fd2826708
Markus Alexander Kuppe <kuppe-github.com@lemmster.de>
2013-04-18 12:08:21 +0200

    Exercise 58.3 Adjust handler

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handler/NewTodoHandler.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handler/NewTodoHandler.java
@@ -5,6 +5,7 @@ import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Shell;
 
+import com.example.e4.rcp.todo.model.ITodoService;
 import com.example.e4.rcp.todo.model.Todo;
 import com.example.e4.rcp.todo.wizard.ConfirmationWizardPage;
 import com.example.e4.rcp.todo.wizard.TodoWizard;
@@ -12,7 +13,7 @@ import com.example.e4.rcp.todo.wizard.TodoWizardPage;
 
 public class NewTodoHandler {
 	@Execute
-	public void execute(Shell shell) {
+	public void execute(Shell shell, ITodoService model) {
 		Wizard wizard = new TodoWizard();
 		wizard.setWindowTitle("New Todo Window Title");
 
@@ -23,7 +24,7 @@ public class NewTodoHandler {
 
 		WizardDialog dialog = new WizardDialog(shell, wizard);
 		if (dialog.open() == WizardDialog.OK) {
-			System.out.println(todo);
+			model.saveTodo(todo);
 		}
 	}
 }
