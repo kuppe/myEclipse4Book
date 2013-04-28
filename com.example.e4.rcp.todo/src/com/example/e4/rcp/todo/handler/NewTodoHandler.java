e5a75ce4ab48745f9a2d21a50ccd768c171fb9af
Markus Alexander Kuppe <kuppe-github.com@lemmster.de>
2013-04-28 10:42:05 +0200

    Exercise 104 Dependency injection for your own object (TodoWizard)

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handler/NewTodoHandler.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handler/NewTodoHandler.java
@@ -4,27 +4,32 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.e4.core.contexts.ContextInjectionFactory;
+import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Shell;
 
 import com.example.e4.rcp.todo.model.ITodoService;
 import com.example.e4.rcp.todo.model.Todo;
-import com.example.e4.rcp.todo.wizard.ConfirmationWizardPage;
 import com.example.e4.rcp.todo.wizard.TodoWizard;
-import com.example.e4.rcp.todo.wizard.TodoWizardPage;
 
+@SuppressWarnings("restriction")
 public class NewTodoHandler {
 	@Execute
-	public void execute(Shell shell, final ITodoService model) {
-		Wizard wizard = new TodoWizard();
-		wizard.setWindowTitle("New Todo Window Title");
+	public void execute(Shell shell, final ITodoService model,
+			MApplication application) {
 
 		final Todo todo = new Todo();
 
-		wizard.addPage(new TodoWizardPage("New Todo", todo));
-		wizard.addPage(new ConfirmationWizardPage("Confirm Todo"));
+		IEclipseContext context = application.getContext();
+		IEclipseContext childContext = context.createChild();
+		childContext.set(Todo.class, todo);
+
+		Wizard wizard = ContextInjectionFactory.make(TodoWizard.class,
+				childContext);
 
 		WizardDialog dialog = new WizardDialog(shell, wizard);
 		if (dialog.open() == WizardDialog.OK) {
--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/wizard/TodoWizard.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/wizard/TodoWizard.java
@@ -1,9 +1,27 @@
 package com.example.e4.rcp.todo.wizard;
 
+import javax.inject.Inject;
+
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
 
+import com.example.e4.rcp.todo.model.Todo;
+
 public class TodoWizard extends Wizard {
+
+	@Inject
+	private Todo todo;
+
+	public TodoWizard() {
+		setWindowTitle("New Todo Window Title");
+	}
+
+	@Override
+	public void addPages() {
+		addPage(new TodoWizardPage("New Todo", todo));
+		addPage(new ConfirmationWizardPage("Confirm Todo"));
+		super.addPages();
+	}
 
 	@Override
 	public boolean canFinish() {
